4b2f3faffba31ad2c2d1edadb5c9f52bcb9cf065
Laurie Williams <laurie.huw.williams@googlemail.com>
2017-03-20 14:54:03 +0700

    format account specs

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe Account do
 
+  # ----- Validations -----
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:subdomain) }
   it { is_expected.to validate_uniqueness_of(:subdomain) }
-
 end
